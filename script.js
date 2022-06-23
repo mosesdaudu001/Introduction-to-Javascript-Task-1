15976f4ccbd2f6c588b9da2afc527c9807845db6
Moses Daudu <mosesdaudu001@gmail.com>
2022-06-23 08:42:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var name = window.prompt("What Is Your Name?");
+var height = window.prompt("What Is Your Height?");
+var country = window.prompt("What Country Are You From?");
+
+alert("Your name is"+" "+name+", "+"Your height is"+" "+height+", "+"and you are from"+" "+ country);
+console.log("Your name is"+" "+name+", "+"Your height is"+" "+height+", "+"and you are from"+" "+ country)
